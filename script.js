15c24ea80b338f2e5f27d64ed312507a79c232d0
PerezLyncoln <lyncolnperez21@gmail.com>
2026-03-17 16:40:00 +0800

    exploring js 0.1

---
+++ script.js
@@ -0,0 +1,4 @@
+function change_section() {
+    document.getElementsByClassName("image_1")[0].src = "images/ishowspeed-ishowspeed-fortnite.gif";
+    document.getElementsByClassName("target_1")[0].innerText = "Man Speed Locked In!";
+}
